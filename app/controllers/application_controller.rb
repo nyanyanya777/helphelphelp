aca294f0d990ea61cfd8c543bb44b3783f849b48
kohei <kohei@koheinoAir.lan>
2017-04-20 17:03:22 +0900

    devise-migration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   PERMISSIBLE_ATTRIBUTES = %i(name)
 
   protected
+  def configure_permitted_parameters
   devise_parameter_sanitizer.permit(:sign_up, keys: PERMISSIBLE_ATTRIBUTES)
   devise_parameter_sanitizer.permit(:account_update, keys: PERMISSIBLE_ATTRIBUTES)
   end
